b5dcf599a38fa7d6294f5aadba3bc233cc8f3a36
garrettmills <shout@garrettmills.dev>
2021-11-29 16:04:09 -0600

    Add reporting file for standard error messages

---
+++ src/Reporting.h
@@ -0,0 +1,36 @@
+#ifndef SWARMC_REPORTING_H
+#define SWARMC_REPORTING_H
+
+#include <string>
+#include "shared/util/Console.h"
+#include "lang/Position.h"
+
+namespace swarmc {
+
+    class Reporting {
+    public:
+        static void nameError(const Lang::Position* pos, std::string message) {
+            Console::get()
+                ->bold()->color("red")->print("[Name Error] ")->reset()
+                ->bold()->print(pos->start() + " ")->reset()
+                ->line(message);
+        }
+
+        static void typeError(const Lang::Position* pos, std::string message) {
+            Console::get()
+                ->bold()->color("red")->print("[Type Error] ")->reset()
+                ->bold()->print(pos->start() + " ")->reset()
+                ->line(message);
+        }
+
+        static void syntaxError(const Lang::Position* pos, std::string message) {
+            Console::get()
+                ->bold()->color("red")->print("[Syntax Error] ")->reset()
+                ->bold()->print(pos->start() + " ")->reset()
+                ->line(message);
+        }
+    };
+
+}
+
+#endif
